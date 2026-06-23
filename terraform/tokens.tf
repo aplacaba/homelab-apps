dc15382b68a5ea3c28a7cfb81dcee11e7593bf07
aplacaba <alacaba@fastmail.com>
2026-06-23 18:17:12 +0800

    fix: api token resources field is string, not map

--- terraform/tokens.tf
+++ terraform/tokens.tf
@@ -4,9 +4,7 @@ resource "cloudflare_api_token" "cert_manager_watchtoken" {
     {
       effect            = "allow"
       permission_groups = [{ id = "Zone Read" }, { id = "DNS Edit" }]
-      resources = {
-        "com.cloudflare.api.account.zone.${data.cloudflare_zone.watchtoken_org.zone_id}" = "*"
-      }
+      resources         = "com.cloudflare.api.account.zone.${data.cloudflare_zone.watchtoken_org.zone_id}"
     }
   ]
 }
@@ -17,9 +15,7 @@ resource "cloudflare_api_token" "cert_manager_alacaba" {
     {
       effect            = "allow"
       permission_groups = [{ id = "Zone Read" }, { id = "DNS Edit" }]
-      resources = {
-        "com.cloudflare.api.account.zone.${data.cloudflare_zone.alacaba_org.zone_id}" = "*"
-      }
+      resources         = "com.cloudflare.api.account.zone.${data.cloudflare_zone.alacaba_org.zone_id}"
     }
   ]
 }
