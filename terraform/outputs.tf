c2b423d8ab4d69a735db4958baa79f4d64a6e39d
aplacaba <alacaba@fastmail.com>
2026-06-23 17:42:46 +0800

    chore: format terraform outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,17 @@
 output "tunnel_token" {
-  value     = cloudflare_tunnel.main.tunnel_token
-  sensitive = true
+  value       = cloudflare_tunnel.main.tunnel_token
+  sensitive   = true
   description = "Token for cloudflared deployment"
 }
 
 output "watchtoken_api_token" {
-  value     = cloudflare_api_token.cert_manager_watchtoken.value
-  sensitive = true
+  value       = cloudflare_api_token.cert_manager_watchtoken.value
+  sensitive   = true
   description = "API token for cert-manager DNS-01 on watchtoken.org"
 }
 
 output "alacaba_api_token" {
-  value     = cloudflare_api_token.cert_manager_alacaba.value
-  sensitive = true
+  value       = cloudflare_api_token.cert_manager_alacaba.value
+  sensitive   = true
   description = "API token for cert-manager DNS-01 on alacaba.org"
 }
