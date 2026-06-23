37ec456937fda50a46d08900f81ea858d8befb87
aplacaba <alacaba@fastmail.com>
2026-06-24 01:01:13 +0800

    fix: use data source for permission group IDs

--- terraform/tokens.tf
+++ terraform/tokens.tf
@@ -1,9 +1,22 @@
+data "cloudflare_api_token_permission_groups_list" "all" {}
+
+locals {
+  zone_read_id = one([
+    for g in data.cloudflare_api_token_permission_groups_list.all.result : g.id
+    if g.name == "Zone Read"
+  ])
+  dns_edit_id = one([
+    for g in data.cloudflare_api_token_permission_groups_list.all.result : g.id
+    if g.name == "DNS Edit"
+  ])
+}
+
 resource "cloudflare_api_token" "cert_manager_watchtoken" {
   name = "cert-manager-watchtoken-org"
   policies = [
     {
       effect            = "allow"
-      permission_groups = [{ id = "Zone Read" }, { id = "DNS Edit" }]
+      permission_groups = [{ id = local.zone_read_id }, { id = local.dns_edit_id }]
       resources = jsonencode({
         "com.cloudflare.api.account.zone.${data.cloudflare_zone.watchtoken_org.zone_id}" = "*"
       })
@@ -16,7 +29,7 @@ resource "cloudflare_api_token" "cert_manager_alacaba" {
   policies = [
     {
       effect            = "allow"
-      permission_groups = [{ id = "Zone Read" }, { id = "DNS Edit" }]
+      permission_groups = [{ id = local.zone_read_id }, { id = local.dns_edit_id }]
       resources = jsonencode({
         "com.cloudflare.api.account.zone.${data.cloudflare_zone.alacaba_org.zone_id}" = "*"
       })
